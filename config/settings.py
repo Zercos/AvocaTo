193e426b8f08701b9d71b9337c50882302d57223
Maksym Hryhoriev <zercos2106@gmail.com>
2020-11-24 20:36:01 +0100

    Remove the Django secret key from settings

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^x5x_l_fh5c!6(!!&%6id9he$8yn7h^&g(!v6g8%fy0z1d&(-r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
