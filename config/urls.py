62fe2141d3b0a8bb4a04773200dfe3d85a6e9725
Maksym Hryhoriev <zercos2106@gmail.com>
2020-12-01 18:24:21 +0100

    Add the authentication for users

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+import quest.urls
+import users.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(quest.urls, namespace='quest')),
+    path('users/', include(users.urls, namespace='users')),
 ]
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from users import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+]
