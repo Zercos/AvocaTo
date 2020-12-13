6d5375f6342c32a68a775ed55e8d5d41ffe5830a
Maksym Hryhoriev <zercos2106@gmail.com>
2020-12-13 22:02:26 +0100

    Add the DailyQuestionList and TodaysQuestionList

--- quest/views.py
+++ quest/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.utils import timezone
+from django.views.generic import DayArchiveView, RedirectView
 
-# Create your views here.
+from quest.models import Question
+
+
+class DailyQuestionList(DayArchiveView):
+    queryset = Question.objects.all()
+    date_field = 'created'
+    month_format = '%m'
+    allow_empty = True
+
+
+class TodaysQuestionList(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        today = timezone.now()
+        return reverse('quest:daily_questions', kwargs={
+            'day': today.day,
+            'month': today.month,
+            'year': today.year
+        })
