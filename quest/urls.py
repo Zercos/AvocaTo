421ac18b5451353e7a244dab348c8ff152d873c1
Maksym Hryhoriev <zercos2106@gmail.com>
2021-01-12 20:27:26 +0100

    Accept the answers to questions

--- quest/urls.py
+++ quest/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('daily/<int:year>/<int:month>/<int:day>/', views.DailyQuestionList.as_view(), name='daily_questions'),
     path('q/<int:pk>', views.QuestionDetailView.as_view(), name='question_detail'),
     path('ask', views.AskQuestionView.as_view(), name='ask'),
+    path('a/<int:pk>/accept', views.UpdateAnswerAcceptanceView.as_view(), name='update_answer_acceptance'),
     path('q/<int:pk>/answer', views.CreateAnswerView.as_view(), name='answer_question'),
 ]
